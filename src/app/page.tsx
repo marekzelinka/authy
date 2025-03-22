0b73c2e4d257fdc3f89109a5a419ad6801150b62
Marek Zelinka <mzelinka17@gmail.com>
2025-03-22 16:57:51 +0100

    fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,8 @@ export default async function Home() {
       <div className="mx-auto w-full max-w-7xl overflow-hidden">
         <div className="relative min-h-96 p-6">
           <div className="space-y-4">
-            <h1 className="text-brand-secondary text-xl font-bold">
-              Welcome to Authy!
-            </h1>
-            <p className="text-brand-secondary/70">
+            <h1 className="text-xl font-bold">Welcome to Authy!</h1>
+            <p className="text-muted-foreground">
               This is a public page accessible to everyone.
             </p>
           </div>
