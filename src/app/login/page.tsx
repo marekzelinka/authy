f2f865b7a02022a2885b94cbd6c98a6b07de8685
Marek Zelinka <mzelinka17@gmail.com>
2025-03-23 14:10:03 +0100

    using middleware to protect routes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,9 @@
 import { AuthPanel } from "@/components/auth-panel";
 import { LoginForm } from "@/components/login-form";
 import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
 
 export default async function Login() {
   const session = await auth();
-
-  if (session) {
-    redirect("/");
-  }
 
   return (
     <div className="min-h-screen">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,37 @@
-export { auth as middleware } from "@/lib/auth";
+import { auth } from "@/lib/auth";
+import {
+  NextResponse,
+  type MiddlewareConfig,
+  type NextMiddleware,
+} from "next/server";
+
+export const middleware: NextMiddleware = async (request) => {
+  const session = await auth();
+  const protectedRoutes = [
+    "/dashboard",
+    "/dashboard/settings",
+    "/dashboard/profile",
+  ];
+
+  const isProtectedRoute = protectedRoutes.map((route) =>
+    request.nextUrl.pathname.startsWith(route),
+  );
+
+  if (!isProtectedRoute && !session) {
+    const redirectUrl = new URL("/login", request.url);
+
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (!session && isProtectedRoute) {
+    const redirectUrl = new URL("/login", request.url);
+
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/api/dashboard/:path*"],
+} satisfies MiddlewareConfig;
